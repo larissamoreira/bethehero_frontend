22ed69e564490795ae028b69f47485b986c4cfda
Larissa Gusmao <larissamgusmao@gmail.com>
2020-04-23 23:36:23 -0400

    Updates

---
+++ precache-manifest.92d4491ba10ece1c588b9839c40f44fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ab40579da8dfcf133b986ec17d5d6c",
+    "url": "/bethehero_frontend/index.html"
+  },
+  {
+    "revision": "89682d658795b5f357c8",
+    "url": "/bethehero_frontend/static/css/main.42bf6f39.chunk.css"
+  },
+  {
+    "revision": "3cc785904fc578377359",
+    "url": "/bethehero_frontend/static/js/2.a9d54de6.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/bethehero_frontend/static/js/2.a9d54de6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89682d658795b5f357c8",
+    "url": "/bethehero_frontend/static/js/main.7f93cfa1.chunk.js"
+  },
+  {
+    "revision": "7cb5f65a958a4369efe4",
+    "url": "/bethehero_frontend/static/js/runtime-main.46379ed6.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/bethehero_frontend/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "/bethehero_frontend/static/media/logo.7eea718b.svg"
+  }
+]);
